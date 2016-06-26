cdc8d883c118c5d4ad3f5bfbccae99a3251c6600
alexboklin <alex.boklin@gmail.com>
2016-06-26 11:01:16 +0300

    add some props and set their types

--- App.js
+++ App.js
@@ -3,8 +3,30 @@ import React from 'react';
 class App extends React.Component {
 
     render() {
-        return <div>Hi there!</div>;
+    	let txtProvided = this.props.txtProvided
+
+        return (
+        	<div>
+        		<h1>Hi there!</h1>
+        		<h2>And this is... {txtProvided}</h2>
+        		<h3>... and this is... {this.props.txtDefault}</h3>
+        	</div>	
+    	)
+   
     }
+
 }
+
+App.propTypes = {
+	txtProvided: React.PropTypes.string.isRequired,
+	txtDefault: React.PropTypes.string
+}
+
+App.defaultProps = {
+	txtDefault: "default txt prop"
+}
+
+// stateless function component
+// const App = () => <h1>Oh, hi there!</h1>
 
 export default App
