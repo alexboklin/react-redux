1bd67202f6846d8ed3d32b2fc93d6e06188620c5
alexboklin <alex.boklin@gmail.com>
2016-07-01 20:47:45 +0300

    turn Slider into a more reusable component

--- App.js
+++ App.js
@@ -7,8 +7,7 @@ class App extends React.Component {
 		this.state = {
 			stateTxt: 'DEFAULT TEXT',
 			red: 0,
-			green: 0,
-			blue: 0,
+
 			buttonValue: 0
 		}
 		this.updateColors = this.updateColors.bind(this)
@@ -24,9 +23,8 @@ class App extends React.Component {
 
 	updateColors(event) {
 		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
+			
 		})
 	}
 
@@ -51,14 +49,17 @@ class App extends React.Component {
         		<hr />
         		
         		{this.state.red}
+        		
         		<br />
-        		<Slider ref="red" update={this.updateColors}/>
-        		{this.state.green}
-        		<br />
-        		<Slider ref="green" update={this.updateColors}/>
-        		{this.state.blue}
-        		<br />
-        		<Slider ref="blue" update={this.updateColors}/>	
+        		<NumInput 
+        			ref="red"
+        			min={0}
+        			max={255}
+        			step={1}
+        			val={+this.state.red} 
+        			label="RED"
+        			// type="number"
+        			update={this.updateColors}/>	
 
         		<br />
         		<Button value={this.state.buttonValue} update={this.updateButtonValue}/>
@@ -78,17 +79,43 @@ App.defaultProps = {
 	txtDefault: "default txt prop",
 }
 
-class Slider extends React.Component {
+class NumInput extends React.Component {
     render() {
+    	let label = this.props.label !== '' ?
+    		<label>{this.props.label} - {this.props.val} </label> : ''
+
         return (
         	<div>
-        		<input ref="inp" type="range" 
-        			min="0"
-        			max="255"	
-        			onChange={this.props.update}/>
+        		<input ref="inp"
+        		type={this.props.type}
+        		min={this.props.min}
+        		max={this.props.max}
+        		step={this.props.step}
+        		defaultValue={this.props.val}
+    			onChange={this.props.update}/>
+    			{label}
         	</div>	
 		)
     }
+}
+
+NumInput.propTypes = {
+	min: React.PropTypes.number,
+	max: React.PropTypes.number,
+	step: React.PropTypes.number,
+	val: React.PropTypes.number,
+	label: React.PropTypes.string,
+	update: React.PropTypes.func.isRequired,
+	type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+	min: 0,
+	max: 0,
+	step: 1,
+	val: 0,
+	label: '',
+	type: 'range'
 }
 
 // stateless function component -- refs won't work with these!
